ce9ca7f9caa94f1097166ab60d24c02c6d4b2180
marc <marc@elearningmedia.es>
2023-11-02 20:35:45 +0100

    remove package with wrong reference

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"bloggo/controller"
+	"bloggo/config"
+	"bloggo/model"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 	"net/http"
 )
@@ -15,8 +17,8 @@ func main() {
 	r.GET("/ping", healthcheck)
 
 	// posts
-	r.POST("/posts", controller.CreatePost)
-	r.GET("/posts", controller.GetPosts)
+	r.POST("/posts", CreatePost)
+	r.GET("/posts", GetPosts)
 	//r.GET("/posts/:id", getPost)
 
 	err := r.Run(":8080")
@@ -28,4 +30,41 @@ func main() {
 
 func healthcheck(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "pong"})
+}
+
+var postCollection = config.GetDBClient().Database("blog").Collection("posts")
+
+func CreatePost(context *gin.Context) {
+	var post model.Post
+	err := context.ShouldBindJSON(&post)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	post = model.New(post.Title, post.Content)
+	_, err = postCollection.InsertOne(context, post)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Post created successfully!", "post": post})
+}
+
+func GetPosts(context *gin.Context) {
+	cursor, err := postCollection.Find(context, bson.D{})
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	var posts []model.Post
+	for cursor.Next(context) {
+		var post model.Post
+		err := cursor.Decode(&post)
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		posts = append(posts, post)
+	}
+	context.JSON(http.StatusOK, gin.H{"posts": posts})
 }
--- model/post.go
+++ model/post.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Post struct {
-	ID      uuid.UUID `json:"id"`
+	ID      string `json:"id"`
 	Title   string    `json:"title"`
 	Content string    `json:"content"`
 	Created string    `json:"created"`
 }
 
 func New(title string, content string) Post {
-	p := Post{uuid.New(), title, content, time.Now().Format("2006-01-02 15:04:05")}
+	p := Post{uuid.New().String(), title, content, time.Now().Format("2006-01-02 15:04:05")}
 	return p
 }
