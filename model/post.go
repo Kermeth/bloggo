5af9ebbe9e9fe600c80b05a4727fff965f0fb8c1
marc <marc@elearningmedia.es>
2023-11-02 21:08:48 +0100

    use NewString instead of New and String for uuid

--- model/post.go
+++ model/post.go
@@ -7,12 +7,12 @@ import (
 
 type Post struct {
 	ID      string `json:"id"`
-	Title   string    `json:"title"`
-	Content string    `json:"content"`
-	Created string    `json:"created"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Created string `json:"created"`
 }
 
 func New(title string, content string) Post {
-	p := Post{uuid.New().String(), title, content, time.Now().Format("2006-01-02 15:04:05")}
+	p := Post{uuid.NewString(), title, content, time.Now().Format("2006-01-02 15:04:05")}
 	return p
 }
